8475acdd2bc4c20df8bc9a02388649d585831d4e
Alfonso Gonzalez <alfonsog@gmail.com>
2019-06-18 13:08:29 +0100

    Change the explanation and comments for problem 1 based on changes required and suggested by reviewer

--- problem_1.py
+++ problem_1.py
@@ -21,17 +21,27 @@ def sqrt(number):
 
     # We iterate while lower bound is not higher than upper bound
     while lower_bound <= upper_bound:
+        # calculate the midpoint which is the potential floored square number we are looking for
         midpoint = (upper_bound + lower_bound) // 2
+        # calculate the power of two from midpoint
         power_of_two = midpoint * midpoint
+        # if the power of two of midpoint is equals to number we have our number and we return it
         if power_of_two == number:
             return midpoint
         elif power_of_two > number:
+            # if the power of two from midpoint is higher than number we store this value in latest_upbound
+            # for comparison at the end and update the upper_bound
             latest_upbound = midpoint
             upper_bound = midpoint - 1
         else:
+            # the power of two from midpoint is lower than number so we store this value in latest_lobound
+            # for comparison at the end and update the lower_bound
             latest_lobound = midpoint
             lower_bound = midpoint + 1
 
+    # at this point, we have two candidates the latest_upbound and the latest_lobound
+    # if latest_upbound power of two is bigger than number, I return latest_lobound
+    # otherwise I return the latest_upbound
     if latest_upbound * latest_upbound > number:
         return latest_lobound
     else:
