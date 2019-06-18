5df50fc88cad173e52de57a13ff68f9a0b2f73e7
Alfonso Gonzalez <alfonsog@gmail.com>
2019-06-18 17:28:59 +0100

    Problem 4. Refactor the solution to this problem as per reviewer's suggestion. Also update the explanation accordingly. Many thanks

--- explanation_4.md
+++ explanation_4.md
@@ -1,10 +1,14 @@
 # Problem 4. Dutch National Flag
 
-To solve this problem I used three arrays to store the elements based on the number and then I concatenate according
-to the numbers' order, i.e. first 0, then 1 and then 2
+To solve this problem I initially used three arrays to store each of the different numbers but thanks to
+the reviewer's suggestion I ended up using the same array to order the elements or what is called in-place
+sorting.
 
-I loop just once the initial input_list array and based on the elements I add them to the particular array.
-
+I used two pointers, one to point where the index for 0s is and the other to point the index for 2s.
+I loop just once the initial input_list array and based on type of number I swap the current element with 
+ the right pointer, that is, index_0 if it is a 0 or index_2 if it is a two
+ 
 The runtime complexity is O(n) being n the length of the input list which we need to loop once.
 
-The space complexity is also O(n) since the total size of the three new arrays is the length of the input list, n.
+The space complexity is also O(1) that is, running this algorithm the size won't growth since it will only
+use the initial array because the sorting is `in-place`
--- problem_4.py
+++ problem_4.py
@@ -5,23 +5,31 @@ def sort_012(input_list):
     Args:
        input_list(list): List to be sorted
     """
-    # arrays to store the amount of 0 in input_list
-    array0 = []
-    # arrays to store the amount of 1 in input_list
-    array1 = []
-    # arrays to store the amount of 2 in input_list
-    array2 = []
 
-    # iterate through the input_list array and append the elements based on their type
-    for element in input_list:
-        if element == 0:
-            array0.append(element)
-        elif element == 1:
-            array1.append(element)
-        elif element == 2:
-            array2.append(element)
+    # variable to track the position of the 0 values
+    index_0 = 0
 
-    return array0 + array1 + array2
+    # variable to track the position of the 2 values
+    index_2 = len(input_list) - 1
+
+    counter_index = 0
+
+    while counter_index <= index_2:
+        print("counter_index", counter_index, "index 0", index_0, "index_2", index_2)
+
+        if input_list[counter_index] == 0:
+            input_list[counter_index] = input_list[index_0]
+            input_list[index_0] = 0
+            index_0 += 1
+            counter_index += 1
+        elif input_list[counter_index] == 2:
+            input_list[counter_index] = input_list[index_2]
+            input_list[index_2] = 2
+            index_2 -= 1
+        else:
+            counter_index += 1
+
+    return input_list
 
 def test_function(test_case):
     sorted_array = sort_012(test_case)
@@ -31,6 +39,7 @@ def test_function(test_case):
     else:
         print("Fail")
 
-test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
+# test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
 test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
-test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+# test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+# test_function([])
